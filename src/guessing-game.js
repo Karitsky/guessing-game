a6369ba9ed4b5230aff7819c225df2145dff04a9
karitsky <valery.karitsky@gmail.com>
2019-10-02 18:11:19 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.guessing = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessing = Math.round((this.min + this.max) / 2);
+        return this.guessing;
     }
 
     lower() {
-
+        this.max = this.guessing;
+        return this.max;
     }
 
     greater() {
-
+        this.min = this.guessing;
+        return this.min;
     }
+
 }
 
 module.exports = GuessingGame;
